642959753446b34ca979e09c99388ac001db38de
HuanYu <141711079+FisherYuuri@users.noreply.github.com>
2025-04-03 09:45:12 +0800

    Update GasViT.py

--- GasViT.py
+++ GasViT.py
@@ -4,7 +4,67 @@ import torch.nn as nn
 from torch.nn import functional as F
 from einops import rearrange
 
+def conv_1x1_bn(inp, oup):
+    return nn.Sequential(
+        nn.Conv2d(inp, oup, 1, 1, 0, bias=False),
+        nn.BatchNorm2d(oup),
+        nn.LeakyReLU(inplace=True)
+    )
 
+def dwconv_nxn_bn(inp, oup, kernal_size=3, stride=1):
+    padding = int((kernal_size - 1) / 2)
+    return nn.Sequential(
+        nn.Conv2d(inp, oup, kernal_size, stride, padding=padding, groups=inp, bias=False),
+        nn.BatchNorm2d(oup),
+        nn.LeakyReLU(inplace=True)
+    )
+    
+class CoordAtt(nn.Module):
+    def __init__(self, inp, oup, reduction=4):
+        super(CoordAtt, self).__init__()
+        self.pool_h = nn.AdaptiveAvgPool2d((None, 1))
+        self.pool_w = nn.AdaptiveAvgPool2d((1, None))
+
+        mip = max(8, inp // reduction)
+
+        self.conv1 = nn.Conv2d(inp, mip, kernel_size=1, stride=1, padding=0)
+        self.bn1 = nn.BatchNorm2d(mip)
+        self.act = nn.Hardswish(inplace=True)
+
+        self.conv_h = nn.Conv2d(mip, oup, kernel_size=1, stride=1, padding=0)
+        self.conv_w = nn.Conv2d(mip, oup, kernel_size=1, stride=1, padding=0)
+
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.fc = nn.Sequential(
+            nn.Linear(inp, mip, bias=False),
+            nn.ReLU(inplace=True),
+            nn.Linear(mip, inp, bias=False),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        identity = x
+
+        n, c, h, w = x.size()
+        x_h = self.pool_h(x)
+        x_w = self.pool_w(x).permute(0, 1, 3, 2)
+        se = self.avg_pool(x).view(n, c)
+        se = self.fc(se).view(n, c, 1, 1)
+        y = torch.cat([x_h, x_w], dim=2)
+        y = self.conv1(y)
+        y = self.bn1(y)
+        y = self.act(y)
+
+        x_h, x_w = torch.split(y, [h, w], dim=2)
+        x_w = x_w.permute(0, 1, 3, 2)
+        a_h = self.conv_h(x_h).sigmoid()
+        a_w = self.conv_w(x_w).sigmoid()
+
+        out = identity * se.expand_as(identity)
+        out = out * a_w * a_h
+
+        return out
+        
 class MHAttention(nn.Module):
     def __init__(self, inp, oup, reduction=4):
         super(CoordAtt, self).__init__()
